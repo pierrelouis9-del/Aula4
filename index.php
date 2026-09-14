9294e0f08a08e4e2a510d0a1522661a6749ed194
Juvensky Pierre Louis <pierrelouisjuvensky5@gmail.com>
2026-09-14 15:28:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
-echo"<h1>Hello word </h1>
- <p> note que ce n'est qu'un exo mc</p>
- <p> essaye de chercher comment faire des stilisations jesaisque j'aime ce la</p>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    
+    ?>
+</body>
+</html>
