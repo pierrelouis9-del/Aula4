a43bf8c98d9f94f49aa98ed95cf0459579e4bacc
Juvensky Pierre Louis <pierrelouisjuvensky5@gmail.com>
2026-09-14 15:23:58 -0300

    note

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 echo"<h1>Hello word </h1>
- <p> note que ce n'est qu'un exo mc</p>";
+ <p> note que ce n'est qu'un exo mc</p>
+ <p> essaye de chercher comment faire des stilisations jesaisque j'aime ce la</p>";
