812c085d104dbcb1850ab5e0157200f0f556f0f4
Juvensky Pierre Louis <pierrelouisjuvensky5@gmail.com>
2026-09-14 16:54:33 -0300

    bonito

--- index.php
+++ index.php
@@ -1,27 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="index.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
-    <?php 
-    $nome="juvensky";
-    $idade="18";
+    <?php
+    $nome = "juvensky";
+    $idade = "18";
 
-    if ($idade >= 18 ){
-       $adulto="Voce é maior de idade";
+    if ($idade >= 18) {
+        $adulto = "Voce é maior de idade";
     } else {
-        $adulto="Voce é menor de idade";
+        $adulto = "Voce é menor de idade";
     }
     ?>
-    <h1>Nome:<?= $nome ?> </h1>
-    <h2>Idade:<?= $idade ?> </h2>
-    <P><?= $adulto ?></P>
+    <div>
+        <h1>Nome:<?= $nome ?> </h1>
+        <h2>Idade:<?= $idade ?> </h2>
+        <P><?= $adulto ?></P>
+    </div>
 
-    
+
+
 </body>
+
 </html>
